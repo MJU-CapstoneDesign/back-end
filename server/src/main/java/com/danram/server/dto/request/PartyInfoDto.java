9b796ccb795f03ce73e13eca2cffabd306a29628
SeungEon <js112099@gmail.com>
2023-04-08 22:44:37 +0900

    refactor: auth controller

---
+++ server/src/main/java/com/danram/server/dto/request/PartyInfoDto.java
@@ -0,0 +1,22 @@
+package com.danram.server.dto.request;
+
+import lombok.*;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+public class PartyInfoDto {
+    private String groupType; // 모집 타입
+    private String groupName; // 모임 이름
+    private String description; // 모집 상세 글
+    private Long max; // 모집 최대 인원
+    private Date startAt; // 시작알
+    private Date endAt; // 종료일
+    private String Location; // 위치(시 구 동)
+    private String alarmFrequency; // 알람 빈도(월, 화, 수)
+    private String alarmTime; //알람 시간
+}
