7c8e8c23ce010a6ba7675073658f7032ae1e234d
SeungEon <js112099@gmail.com>
2023-04-14 19:10:20 +0900

    refactor: api 설명 추가

--- server/src/main/java/com/danram/server/controller/FCMController.java
+++ server/src/main/java/com/danram/server/controller/FCMController.java
@@ -5,6 +5,9 @@ import com.danram.server.service.fcm.FCMService;
 import com.danram.server.service.fcm.FCMServiceImpl;
 import com.danram.server.service.member.MemberService;
 import com.danram.server.util.JwtUtil;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +23,13 @@ public class FCMController {
     private final FCMService fcmService;
 
     @PostMapping("/alarm")
+    @ApiOperation("fcm 예제 api")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정상 응답"),
+            @ApiResponse(responseCode = "404", description = "해당 정보를 가진 Member가 없음"),
+            @ApiResponse(responseCode = "403", description = "해당 사용자가 Member 권한이 아님"),
+            @ApiResponse(responseCode = "401", description = "해당 사용자가 인증되지 않음 | 토큰 만료")
+    })
     public ResponseEntity pushAlarm(@RequestBody FCMDto fcmDto) throws IOException {
         System.out.println(fcmDto.getTopic() + " "
                 + fcmDto.getTitle() + " " + fcmDto.getBody());
@@ -32,6 +42,13 @@ public class FCMController {
     }
 
     @GetMapping("/alarm/{partyId}")
+    @ApiOperation("fcm 알람 api")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정상 응답"),
+            @ApiResponse(responseCode = "404", description = "해당 정보를 가진 Member가 없음"),
+            @ApiResponse(responseCode = "403", description = "해당 사용자가 Member 권한이 아님"),
+            @ApiResponse(responseCode = "401", description = "해당 사용자가 인증되지 않음 | 토큰 만료")
+    })
     public ResponseEntity pushAlarm(@PathVariable(name = "partyId") Long partyId) throws IOException {
         fcmService.sendAlarm(partyId);
 
