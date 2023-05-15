280d6adb6efddacea85e75bc07e83b6d9bc704c1
Qkdrmf <djs9844@naver.com>
2023-05-15 14:24:11 +0900

    fix: create party parameter

--- server/src/main/java/com/danram/server/controller/PartyController.java
+++ server/src/main/java/com/danram/server/controller/PartyController.java
@@ -35,7 +35,7 @@ public class PartyController {
             @ApiResponse(responseCode = "403", description = "해당 사용자가 Member 권한이 아님"),
             @ApiResponse(responseCode = "401", description = "해당 사용자가 인증되지 않음 | 토큰 만료")
     })
-    public ResponseEntity<PartyMembers> createParty(@RequestBody PartyInfoDto partyInfoDto, @RequestParam(value = "file")MultipartFile file) throws IOException {
+    public ResponseEntity<PartyMembers> createParty(PartyInfoDto partyInfoDto, @RequestParam(value = "file")MultipartFile file) throws IOException {
         if (file.isEmpty()) {
             throw new RuntimeException("File is not exist");
         }
